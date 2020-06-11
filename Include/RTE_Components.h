1197bd91bca0706c5c627f8df4dfb690202cef21
Maximilian Gerhardt <maxi.gerhardt@googlemail.com>
2020-06-11 18:57:31 +0200

    Remove incorrect comment

--- Include/RTE_Components.h
+++ Include/RTE_Components.h
@@ -3,9 +3,7 @@
 
 /*
  * Define the Device Header File
- * Here specific for our stm32f1
  */
-//#define CMSIS_device_header "stm32f1xx.h"
 #define CMSIS_device_header "stm32_def.h"
 
 #endif /* RTE_COMPONENTS_H */
