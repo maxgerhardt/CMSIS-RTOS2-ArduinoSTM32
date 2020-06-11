4c5fa91d293f61c23b5aecec6df70e7201eb86fa
Maximilian Gerhardt <maxi.gerhardt@googlemail.com>
2020-06-11 19:11:21 +0200

    Flatten library structure

--- compile_correct_irq.py
+++ compile_correct_irq.py
@@ -8,19 +8,28 @@ import sys
 
 board_cpu = env.BoardConfig().get("build.cpu")
 
+# list of all supported IRQ files. exclude the ones we do not want. 
+irq_files = [
+	"irq_cm0.S",
+	"irq_cm3.S",
+	"irq_cm4f.S"
+]
+
 irq_file = None 
 if board_cpu == "cortex-m0": 
-    irq_file = "RTX/Source/GCC/irq_cm0.S"
+    irq_file = "src/irq_cm0.S"
 elif board_cpu == "cortex-m3":
-    irq_file = "RTX/Source/GCC/irq_cm3.S"
+    irq_file = "src/irq_cm3.S"
 # cortex m7 is closer to cortex m4f.
 # validation pending: https://github.com/ARM-software/CMSIS_5/issues/943
 elif board_cpu == "cortex-m4" or board_cpu == "cortex-m7":
-    irq_file = "RTX/Source/GCC/irq_cm4f.S"
+    irq_file = "src/irq_cm4f.S"
 else: 
     sys.stderr.write("Cannot deduce correct IRQ source file for given build.cpu \"%s\". Build will fail.\n" % board_cpu)
     # default to Cortex M3..
-    irq_file = "RTX/Source/GCC/irq_cm3.S"
+    irq_file = "src/irq_cm3.S"
 
-# just modify the source filter. must still manually excluse examples though -- otherwise it will compile it.
-env.Replace(SRC_FILTER=["+<*>", "-<.git/>", "-<examples/>", "-<RTX/Source/GCC/*>", "+<%s>" % irq_file])
+# build list of excluded IRQ files 
+excluded_irqs = ["-<%s>" % irq for irq in irq_files if irq != irq_file]
+# append it to the blacklist
+env.Append(SRC_FILTER=excluded_irqs)
